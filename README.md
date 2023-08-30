8372e48503bd472bfe1ee98382c9b4b0a52bf812
Spiro Metaxas <spirometaxas@gmail.com>
2023-08-30 19:10:40 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# periodic-table-cli-py
-[BETA] An interactive Periodic Table of Elements app for the console
+# periodic-table-cli
+
+[BETA] An interactive Periodic Table of Elements app for the console!
+
+Under construction, check back soon!
---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+def get_readme():
+    with open("README.md", encoding="utf8") as f:
+        return f.read()
+
+setup(
+  name = 'periodic-table-cli',
+  packages = ['periodic_table_cli'],
+  version = '0.0.1',
+  license='MIT',
+  description = '[BETA] An interactive Periodic Table of Elements app for the console!',
+  long_description=get_readme(),
+  long_description_content_type="text/markdown",
+  author = 'Spiro Metaxas',
+  author_email = 'spirometaxas@outlook.com',
+  url = 'https://spirometaxas.com/projects/periodic-table-cli/',
+  download_url = 'https://github.com/spirometaxas/periodic-table-cli-py/archive/refs/tags/v0.0.1.tar.gz',
+  keywords = [ 'Periodic Table', 'Periodic Table of Elements', 'periodic', 'periodic-table-cli', 'Chemistry', 'elements', 'atoms', 'atomic', 'cli', 'console', 'terminal', 'shell', 'unicode' ],
+  entry_points={"console_scripts": ["periodic-table-cli=periodic_table_cli.cli:main"]},
+  classifiers=[
+    'Development Status :: 4 - Beta',
+
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+
+    'Topic :: Scientific/Engineering :: Chemistry',
+
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    "Operating System :: OS Independent",
+  ],
+  include_package_data=True,
+  python_requires='>=3.0',
+)

13cb41c3b1e132e78497b3a42cf63dff4b106a6c
Spiro Metaxas <spirometaxas@gmail.com>
2023-12-22 23:31:40 -0500

    Updated README

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ def get_install_requirements():
 setup(
   name = 'periodic-table-cli',
   packages = ['periodic_table_cli'],
-  version = '0.0.2',
+  version = '1.0.0',
   license='MIT',
-  description = '[BETA] An interactive Periodic Table of Elements app for the console!',
+  description = 'An interactive Periodic Table of Elements app for the console!',
   long_description_content_type='text/markdown',
   long_description=get_readme(),
   author = 'Spiro Metaxas',
   author_email = 'spirometaxas@outlook.com',
   url = 'https://spirometaxas.com/projects/periodic-table-cli/',
-  download_url = 'https://github.com/spirometaxas/periodic-table-cli-py/archive/refs/tags/v0.0.2.tar.gz',
+  download_url = 'https://github.com/spirometaxas/periodic-table-cli-py/archive/refs/tags/v1.0.0.tar.gz',
   project_urls={
     'Source Code': 'https://github.com/spirometaxas/periodic-table-cli-py',
   },
@@ -29,18 +29,24 @@ setup(
   entry_points={'console_scripts': ['periodic-table-cli=periodic_table_cli.cli:main']},
   install_requires=get_install_requirements(),
   classifiers=[
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
+    'Environment :: Console :: Curses',
 
     'Intended Audience :: Education',
     'Intended Audience :: Science/Research',
 
+    'Topic :: Education',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
     'Topic :: Scientific/Engineering :: Chemistry',
+    'Topic :: Terminals',
 
     'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.7',
     'Operating System :: OS Independent',
   ],
   include_package_data=True,
-  python_requires='>=3.0',
+  python_requires='>=2.7',
 )
